531c64b744b83d973b45b138cacc5f71b0e946d6
Gaurav1504333 <114723172+Gaurav1504333@users.noreply.github.com>
2024-04-28 09:48:12 +0530

    deploy

--- hawaii.js
+++ hawaii.js
@@ -58,7 +58,7 @@ app.use(bodyParser.urlencoded({ extended: true })); // Parse URL-encoded bodies
 app.use(express.static(__dirname));
 
 // Create routes dynamically for kauais
-const kauais = ['hotelkauai1', 'hotelkauai2', 'hotelkauai3', 'hotelkauai4', 'hotelkauai5', 'hotelkauai6'];
+const kauais = ['hotelkauai1', 'hotelkauai2', 'hotelkauai3'];
 kauais.forEach((kauai, index) => {
     const submitAction = `submitkauai${index + 1}`;
     app.get(`/${kauai}`, (req, res) => {
@@ -68,7 +68,7 @@ kauais.forEach((kauai, index) => {
 });
 
 // Create routes dynamically for oahus
-const oahus = ['hoteloahu1', 'hoteloahu2', 'hoteloahu3', 'hoteloahu4', 'hoteloahu5', 'hoteloahu6'];
+const oahus = ['hoteloahu1', 'hoteloahu2', 'hoteloahu3'];
 oahus.forEach((oahu, index) => {
     const submitAction = `submitoahu${index + 1}`;
     app.get(`/${oahu}`, (req, res) => {
@@ -78,13 +78,43 @@ oahus.forEach((oahu, index) => {
 });
 
 // Create routes dynamically for molokais
-const molokais = ['hotelmolokai1', 'hotelmolokai2', 'hotelmolokai3', 'hotelmolokai4', 'hotelmolokai5', 'hotelmolokai6'];
+const molokais = ['hotelmolokai1', 'hotelmolokai2', 'hotelmolokai3'];
 molokais.forEach((molokai, index) => {
     const submitAction = `submitmolokai${index + 1}`;
     app.get(`/${molokai}`, (req, res) => {
         res.sendFile(path.join(__dirname, `${molokai}.html`));
     });
     app.post(`/${submitAction}`, createRouteHandler(createBookingModel(`hotelmolokai${index + 1}data`)));
+});
+
+// Create routes dynamically for lanais
+const lanais = ['hotellanai1', 'hotellanai2', 'hotellanai3'];
+lanais.forEach((lanai, index) => {
+    const submitAction = `submitlanai${index + 1}`;
+    app.get(`/${lanai}`, (req, res) => {
+        res.sendFile(path.join(__dirname, `${lanai}.html`));
+    });
+    app.post(`/${submitAction}`, createRouteHandler(createBookingModel(`hotellanai${index + 1}data`)));
+});
+
+// Create routes dynamically for mauis
+const mauis = ['hotelmaui1', 'hotelmaui2', 'hotelmaui3'];
+mauis.forEach((maui, index) => {
+    const submitAction = `submitmaui${index + 1}`;
+    app.get(`/${maui}`, (req, res) => {
+        res.sendFile(path.join(__dirname, `${maui}.html`));
+    });
+    app.post(`/${submitAction}`, createRouteHandler(createBookingModel(`hotelmaui${index + 1}data`)));
+});
+
+// Create routes dynamically for iohs
+const iohs = ['hotelioh1', 'hotelioh2', 'hotelioh3'];
+iohs.forEach((ioh, index) => {
+    const submitAction = `submitioh${index + 1}`;
+    app.get(`/${ioh}`, (req, res) => {
+        res.sendFile(path.join(__dirname, `${ioh}.html`));
+    });
+    app.post(`/${submitAction}`, createRouteHandler(createBookingModel(`hotelioh${index + 1}data`)));
 });
 
 app.get('/css.css', (req, res) => {
