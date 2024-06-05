7711f95c545cf017098fafd42d9b32e684df2835
Gaurav1504333 <114723172+Gaurav1504333@users.noreply.github.com>
2024-06-05 15:01:49 +0530

    deploy

--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,19 +1,4 @@
-app.post('/submit', async (req, res) => {
-    try {
-        const { fullname, phone, arrival_date, leaving_date } = req.body;
-
-        const booking = new Booking({
-            fullname,
-            phone,
-            arrival_date,
-            leaving_date
-        });
-
-        await booking.save();
-
-        res.send('Booking successfully added to the database.');
-    } catch (error) {
-        console.error('Error adding booking:', error);
-        res.status(500).send('Internal Server Error');
-    }
+const server = app.listen(() => {
+    const port = server.address().port;
+    console.log(`Server running at http://localhost:${port}/`);
 });
